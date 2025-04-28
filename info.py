bf29993ea26c6d0f53b251514fbecfc01037301e
CHENYANG <168895666+ChenYnag0009@users.noreply.github.com>
2025-04-28 07:37:35 +0700

    Update info.py

--- info.py
+++ info.py
@@ -5,12 +5,13 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+API_ID = int(environ.get('API_ID', '17822365'))
+API_HASH = environ.get('API_HASH', 'b561a326f3d3e7e6ef41cf7d57ab8fb3')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7951520969:AAE0Lb-lCglr8HD0CBfCrc4B46pt5ywdDB0")
 
 # Bot settings
 PORT = environ.get("PORT", "8080")
+FQDN = environ.get("FQDN", "file2link.sh-vpn.shop")
 
 # Online Stream and Download
 MULTI_CLIENT = False
@@ -23,7 +24,7 @@ else:
 URL = environ.get("URL", "")
 
 # Admins, Channels & Users
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002698541629'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 
 # MongoDB information
@@ -31,6 +32,6 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
